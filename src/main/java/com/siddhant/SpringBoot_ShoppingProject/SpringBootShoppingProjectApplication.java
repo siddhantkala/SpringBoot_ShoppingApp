154920b35a21315f7cd77a98b21789e5e9d3fa76
Siddhant Kala <Siddhant03Kala@gmail.com>
2025-02-09 12:09:35 +0530

    Created Starter code from SpringInitializer

---
+++ src/main/java/com/siddhant/SpringBoot_ShoppingProject/SpringBootShoppingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.siddhant.SpringBoot_ShoppingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShoppingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShoppingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siddhant/SpringBoot_ShoppingProject/SpringBootShoppingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddhant.SpringBoot_ShoppingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShoppingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

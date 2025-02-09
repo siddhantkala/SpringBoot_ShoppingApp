8d7bc29248ee2851a0cc014c280d4a3af76599e4
Siddhant Kala <Siddhant03Kala@gmail.com>
2025-02-10 01:11:14 +0530

    CRUD operations with hardcoded data

--- src/main/java/com/siddhant/SpringBoot_ShoppingProject/controller/ProductController.java
+++ src/main/java/com/siddhant/SpringBoot_ShoppingProject/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.siddhant.SpringBoot_ShoppingProject.controller;
 import com.siddhant.SpringBoot_ShoppingProject.model.Product;
 import com.siddhant.SpringBoot_ShoppingProject.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,14 +13,34 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    //GET
+    @GetMapping("/products")
     public List<Product> getProducts() {
         return service.getProducts();
     }
 
-    @RequestMapping("/product")
-    public Product getProductById(int prodId)
+    @GetMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId)
     {
         return service.getProductById(prodId);
+    }
+
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product product)
+    {
+        service.addProduct(product);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product product)
+    {
+        service.updateProduct(product);
+    }
+
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable int prodId)
+    {
+        service.deleteProduct(prodId);
     }
 }
--- src/main/java/com/siddhant/SpringBoot_ShoppingProject/service/ProductService.java
+++ src/main/java/com/siddhant/SpringBoot_ShoppingProject/service/ProductService.java
@@ -2,15 +2,17 @@ package com.siddhant.SpringBoot_ShoppingProject.service;
 
 import com.siddhant.SpringBoot_ShoppingProject.model.Product;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
-    List<Product> products=Arrays.asList(new Product(1,"Shoes",250),
+    List<Product> products=new ArrayList<>(Arrays.asList(new Product(1,"Shoes",250),
             new Product(2,"Jeans",550),
-            new Product(3,"Shirt",450));
+            new Product(3,"Shirt",450)));
 
     public List<Product> getProducts()
     {
@@ -18,6 +20,34 @@ public class ProductService {
     }
 
     public Product getProductById(int prodId) {
-        
+        return products.stream()
+                .filter(p->p.getProdId()==prodId)
+                .findFirst().get();
+    }
+
+    public void addProduct(Product product) {
+        System.out.println(product);
+        products.add(product);
+    }
+
+    public void updateProduct(Product product) {
+        int index=0;
+        for(int i=0;i<products.size();i++)
+        {
+            if(products.get(i).getProdId()==product.getProdId())
+                index=i;
+        }
+        products.set(index,product);
+    }
+
+    public void deleteProduct(int prodId) {
+        for(int i=0;i<products.size();i++)
+        {
+            if(products.get(i).getProdId()==prodId)
+            {
+                products.remove(i);
+                break;
+            }
+        }
     }
 }
